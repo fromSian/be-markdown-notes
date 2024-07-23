a2809cf2f5f2b9c0317dcde0a9f92c35d3f36363
fromsian <fromSian@163.com>
2024-07-24 01:39:16 +0800

    fix: dynamic import

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
